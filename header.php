d929edc54b1188fd90b8b842f7c4fbe7ad4d1715
mosa0 <m.alsagheer@student.alfa-college.nl>
2022-05-20 18:20:15 +0200

    2nd commit

---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>webshop</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+      crossorigin="anonymous"
+    />
+    <link href="style.css" rel="stylesheet" />
+  </head>
+
+
+
+
+<body>
+    <div class="logo-1">
+<img src="./img/logo.svg" alt="" srcset="">
+    </div>
+    <div class="logo">
+  <img src="./img/twitter_header_photo_2.png" class="img-thumbnail" alt="...">
+    </div>
+    
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">The North Face</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Yeezy</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Tommy Hilfiger</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    </nav>
--- index.php
+++ index.php
@@ -1,64 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>webshop</title>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
-      crossorigin="anonymous"
-    />
-    <link href="style.css" rel="stylesheet" />
-  </head>
+<?php include_once'header.php'
+?>
 
+<!-- Title variabele-->
   <?php 
-  $title = "The North Face Puffer Jackets Line"
+  $title_tnf = "The North Face Puffer Jackets Line"
   ?>
-
-  <body>
-    <div class="logo">
-  <img src="./img/twitter_header_photo_2.png" class="img-thumbnail" alt="...">
-    </div>
-    
-    <nav class="navbar navbar-expand-lg bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">The North Face</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Yeezy</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Tommy Hilfiger</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    </nav>
+   <?php 
+  $title_yeezy = "The Yeezy Line"
+  ?>
+   <?php 
+  $title_tommy = "The Tommy Hilfiger Line"
+  ?>
+  
+  <!-- Prijs Variable-->
+  <?php 
+  $prijs_tnf = "$249,99"
+  ?>
+  
+  
+  <?php 
+  $prijs_yeezy1 = "$599,00"
+  ?>
+  
+  
+  <?php 
+  $prijs_tommy2 = "$110,00"
+  ?>
+  
     <div class="cards">
-      <div class="tnf-t"><h1><?php echo $title?></h1></div>
+      
+      <div class="tnf-t"><h1><?php echo $title_tnf?></h1></div>
       <div class="row row-cols-1 row-cols-md-3 g-4">
         <div class="col">
+        <?php
+          $prijs_tnf = 249.99;
+        ?>
+        <?php
+          $prijs_tnf = 249.99;
+        ?>
           <div class="card">
             <img src="./img/tnf1.jpg" class="card-img-top kaart" alt="..." />
             <div class="card-body">
               <h5 class="card-title">The North Face</h5>
               <p class="card-text">
                 Black Puffer Jacket
+                
+                <h3 class=prijs> <?= number_format($prijs_tnf, 2, ',', '') ?></h3>
+                <button type="button" class="btn btn-dark">Dark</button>
               </p>
             </div>
           </div>
@@ -70,6 +58,9 @@
               <h5 class="card-title">The North Face</h5>
               <p class="card-text">
                 Orange Puffer Jacket
+                
+                <h3 class=prijs> <?= number_format($prijs_tnf, 2, ',', '') ?></h3>
+                <button type="button" class="btn btn-dark">Dark</button>
               </p>
             </div>
           </div>
@@ -81,11 +72,19 @@
               <h5 class="card-title">The North Face</h5>
               <p class="card-text">
                 Yellow Puffer Jacket
+                
+                <h3 class=prijs> <?= number_format($prijs_tnf, 2, ',', '') ?></h3>
+                <button type="button" class="btn btn-dark">Dark</button>
               </p>
             </div>
           </div>
         </div>
-        <div class="yeezy-t"><h1>The Yeezy Line</h1></div>
+
+        <div class="yeezy-t"><h1><?php echo $title_yeezy?></h1></div>
+
+        <?php
+          $prijs_yeezy = 635.00;
+        ?>
         <div class="col">
           <div class="card">
             <img src="./img/yeezy1.jpg" class="card-img-top kaart2" alt="..." />
@@ -93,10 +92,16 @@
               <h5 class="card-title">Yeezy 350 V2</h5>
               <p class="card-text">
                 Black Pirate
+                
+                <h3 class=prijs><?= number_format($prijs_yeezy, 2, ',', '') ?> </h3>
+                <button type="button" class="btn btn-dark">Dark</button>
               </p>
             </div>
           </div>
         </div>
+        <?php
+          $prijs_yeezy = 450.00;
+        ?>
         <div class="col">
           <div class="card">
             <img src="./img/yeezy2.jpg" class="card-img-top kaart2" alt="..." />
@@ -104,71 +109,96 @@
               <h5 class="card-title">Yeezy 350 V2</h5>
               <p class="card-text">
                 Ash Pearle
+                <h3 class=prijs> <?= number_format($prijs_yeezy, 2, ',', '') ?>
               </p>
+              <button type="button" class="btn btn-dark">Dark</button>
             </div>
           </div>
         </div>
         <div class="col">
+        <?php
+          $prijs_yeezy = 439.00;
+        ?>
           <div class="card">
             <img src="./img/yeezy3.jpg" class="card-img-top kaart2" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Yeezy 350 V2</h5>
               <p class="card-text">
                 Oreo
+                
+                <h3 class=prijs><?= number_format($prijs_yeezy, 2, ',', '') ?></h3>
+                <button type="button" class="btn btn-dark">Dark</button>
               </p>
             </div>
           </div>
         </div>
       </div>
-      <div class="tommy-t"><h1>The Tommy Hilfiger Line</h1></div>
+      <div class="tommy-t"><h1><?php echo $title_tommy?></h1></div>
       <div class="row row-cols-1 row-cols-md-2 g-4">
         <div class="col">
+        <?php
+          $prijs_tommy = 99.99;
+        ?>
           <div class="card">
             <img src="./img/tom1.jpg" class="card-img-top kaart3" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Tommy Hilfiger Crewneck</h5>
               <p class="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                White Tommy Hilfiger Crewneck.
+                
+                <h3 class=prijs> <?= number_format($prijs_tommy, 2, ',', '') ?></h3>
+                <button type="button" class="btn btn-dark">Dark</button>
               </p>
             </div>
           </div>
         </div>
         <div class="col">
+        <?php
+          $prijs_tommy = 99.99;
+        ?>
           <div class="card">
             <img src="./img/tom2.jpg" class="card-img-top kaart3" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Tommy Hilfiger crewneck</h5>
               <p class="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+              Green Tommy Hilfiger Crewneck.
+                
+                <h3 class=prijs> <?= number_format($prijs_tommy, 2, ',', '') ?></h3>
+                <button type="button" class="btn btn-dark">Dark</button>
               </p>
             </div>
           </div>
         </div>
         <div class="col">
+        <?php
+          $prijs_tommy = 109.99;
+        ?>
           <div class="card">
             <img src="./img/tom3.jpg" class="card-img-top kaart4" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Tommy Hilfiger Hoodie</h5>
               <p class="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content.
+                Beige Tommy Hilfiger Hoodie.
+                
+                <h3 class=prijs> <?= number_format($prijs_tommy, 2, ',', '') ?></h3>
+                <button type="button" class="btn btn-dark">Dark</button>
               </p>
             </div>
           </div>
         </div>
         <div class="col">
+        <?php
+          $prijs_tommy = 109.99;
+        ?>
           <div class="card">
             <img src="./img/tom4.jpg" class="card-img-top kaart5" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Tommy Hilfiger Hoodie</h5>
               <p class="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                Dark Blue Tommy Hilfiger Hoodie.
+                
+                <h3 class=prijs> <?= number_format($prijs_tommy, 2, ',', '') ?></h3>
+                <button type="button" class="btn btn-dark">Dark</button>
               </p>
             </div>
           </div>
